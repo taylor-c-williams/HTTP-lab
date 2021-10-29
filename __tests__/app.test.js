fec8588c969c487657f3b0e6eae5ee32cab6fb0e
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-29 15:41:06 -0700

    hi and echo tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,19 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+
 describe('app routes', () => {
+  it('Responds to a get method on index with "hi"', async() => {
+    const res = await request(app).get('/');
+    expect (res.text).toEqual('hi');
+  });
+
+  it('/echo returns status 200 and text/plain w req.body', async() => {
+    const res = await request(app).get('/');
+    expect (res.text).toEqual(res.text);
+  });
 
 });
+
+
+
